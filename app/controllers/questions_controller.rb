488e1b4ae58d744caac58b87c49543c1077b8d02
Ekaterina Gorbacheva <ekagorbacheva@mail.ru>
2018-04-02 14:23:54 +0800

    ex 1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question = "Ask you coach anything:"
+  end
+
+  def answer
+    @user_answer = params[:question]
+
+    if @user_answer == "I am going to work"
+      @response = "Great"
+    elsif @user_answer[-1] == "?"
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
